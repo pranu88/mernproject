423c070e49137568d8444db7d0e8bb026c5e255e
pranu88 <pranusha.pothugunta@gmail.com>
2024-01-16 14:22:14 -0500

    Header added

--- src/App.js
+++ src/App.js
@@ -6,24 +6,34 @@ import { getUser } from "./utilities/users-service";
 import AuthPage from "./pages/AuthPage/AuthPage.jsx";
 
 import Navbar from "./components/Navbar";
+import Header from "./components/Header/Header.jsx";
+import LoginForm from "./components/LoginForm/LoginForm.jsx";
+import SignUpForm from "./components/SignUpForm/SignUpForm.jsx";
 
 
 function App() {
   const [user, setUser] = useState(getUser());
   return (
     <div className="App">
-
-      {user ? (
+       <Header/>
+      {/* {user ? (
         <>
           <Navbar user={user} setUser={setUser} />
           <Routes>
-           
-           
+           <Route path="/Login" element ={<LoginForm/>}/>
+           <Route path="/Register" element ={<SignUpForm/>}/>
           </Routes>
         </>
       ) : (
         <AuthPage user={user} setUser={setUser}/>
-      )}
+      )} */}
+
+         <Routes>
+           <Route path="/Login" element ={<LoginForm/>}/>
+           <Route path="/Register" element ={<SignUpForm/>}/>
+          </Routes>
+
+
     </div>
   );
 }
---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import styles from './Header.css'
+
+function Header() {
+  return (
+    <div className='header'>
+        <h1>B-Blog</h1>
+        <div className='styles.links'>
+        <Link style= {{textDecoration:"none", color:"inherit",margin:"1em",fontWeight:"bold"}} to="/Login">Login</Link>
+        <Link style= {{textDecoration:"none", color:"inherit",fontWeight:"bold",margin:"1em"}} to="/Register">Register</Link>
+        </div>
+        
+    </div>
+  )
+}
+
+export default Header
