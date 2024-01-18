0fbf9e9d869fa3a70e32d48d83185ba26306c565
pranu88 <pranusha.pothugunta@gmail.com>
2024-01-18 13:18:39 -0500

    Navbar changed

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -14,14 +14,15 @@ function Navbar({user ,setUser}) {
       <nav style={{ justifyContent: "space-evenly", display: "flex" }}>
 
       <div style={{justifyContent:'space-around'}}>
-        <p style={{margin:'1em'}}>Welcome {user.name}, </p><hr/>        
+        <p style={{margin:'1em'}}>Welcome {user.name}, </p>       
         
-        <Link to="" onClick={handleLogOut}><button>Log-Out</button></Link>
+        
       </div>
 
+        <Link to="/orders/new">Home</Link> 
         <Link to="/orders">Order History</Link> 
-
-        <Link to="/orders/new">New Order</Link> 
+        <Link to="" onClick={handleLogOut}><button>Log-Out</button></Link>
+        
       </nav>
     </>
   );
