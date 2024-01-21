9db41477d6849e494ccc1e551c0f0d98787d5c22
pranu88 <pranusha.pothugunta@gmail.com>
2024-01-20 20:46:41 -0500

    logo changed

--- src/components/Logo/Logo.jsx
+++ src/components/Logo/Logo.jsx
@@ -1,11 +1,11 @@
 import  './Logo.css';
-
+import img from './M.gif'
 export default function Logo() {
 return (
   <div className='Logo'>
-    
-    <div>MERN</div> 
-     <div>MART</div>
+    <img src={img} alt='logo' style={{width:'15vmin',height:'15vmin'}}/>
+    {/* <div>MERN</div> 
+     <div>MART</div> */}
   </div>
 );
 }
